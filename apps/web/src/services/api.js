861f32ef061dc8558cf435290a951eabca9c1808
Solejon_3117 <solejonadashov0@gmail.com>
2026-07-14 12:32:51 +0500

    Update api.js

--- apps/web/src/services/api.js
+++ apps/web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_URL.replace(/\/$/, '')}/api`,
   timeout: 30000,
 });
 
